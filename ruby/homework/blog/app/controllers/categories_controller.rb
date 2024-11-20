9c465aa4c3f8854fd504fb095ecbe132f856f402
Saba Negeye <saban@Sabas-MacBook-Air.local>
2024-11-20 10:41:51 -0600

    added authentication and confirmable

--- ruby/homework/blog/app/controllers/categories_controller.rb
+++ ruby/homework/blog/app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
     
     def index
         @categories = Category.all
